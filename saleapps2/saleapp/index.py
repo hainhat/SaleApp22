52d2ec93cdf26cd5fffe9f31fb5b951054e1d802
unknown <tohainhat17062003@gmail.com>
2024-11-22 17:07:06 +0700

    login

--- saleapps2/saleapp/index.py
+++ saleapps2/saleapp/index.py
@@ -1,19 +1,24 @@
+import math
+
 from flask import render_template, request, redirect
 import dao
-from saleapp import app
+
+from saleapps2.saleapp import app
 
 
 @app.route("/")
 def index():
     q = request.args.get("q")
     cate_id = request.args.get("category_id")
-    products = dao.load_products(q, cate_id)
-    return render_template("index.html", products=products)
+    page = request.args.get("page")
+    pages = dao.count_product()
+    products = dao.load_products(q=q, cate_id=cate_id, page=page)
+    return render_template("index.html", products=products, pages=math.ceil(pages / app.config["PAGE_SIZE"]))
 
 
 @app.route("/products/<int:id>")
 def product_details(id):
-    product = dao.load_product_by_id(id);
+    product = dao.load_product_by_id(id)
     return render_template("product_details.html", product=product)
 
 
@@ -26,14 +31,18 @@ def common_attribute():
 
 @app.route("/login", methods=['get', 'post'])
 def login():
+    err_msg = None
     if request.method.__eq__('POST'):
         print(request.form)
-        username=request.form.get('username')
-        password=request.form.get('password')
-        if username.__eq__('admin') and password.__eq__('123'):
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = dao.auth_user(username=username, password=password)
+        if user:
             return redirect("/")
+        else:
+            err_msg = "Tài khoản hoặc mật khẩu không đúng"
 
-    return render_template("login.html")
+    return render_template("login.html",err_msg=err_msg)
 
 
 if __name__ == '__main__':
--- saleapps2/saleapp/models.py
+++ saleapps2/saleapp/models.py
@@ -1,20 +1,63 @@
-from sqlalchemy import Column, String, Integer
-from saleapp import db, app
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Boolean
+from saleapps2.saleapp import db, app
+from sqlalchemy.orm import relationship
 
 
 class Category(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(100), nullable=True)
+    products = relationship('Product', backref="category", lazy=True)
 
     def __str__(self):
         return self.name
 
 
+class Product(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), unique=True, nullable=False)
+    price = Column(Float, default=0)
+    image = Column(String(300),
+                   default='https://res.cloudinary.com/dy1unykph/image/upload/v1729842193'
+                           '/iPhone_15_Pro_Natural_1_ltf9vr.webp')
+    active = Column(Boolean, default=True)
+    category_id = Column(Integer, ForeignKey(Category.id), nullable=True)
+
+    def __str__(self):
+        return self.name
+
+
+class User(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50))
+    username = Column(String(50), unique=True, nullable=False)
+    password = Column(String(50), nullable=False)
+    active = Column(Boolean, default=True)
+    avatar = Column(String(300),
+                    default='https://res.cloudinary.com/dy1unykph/image/upload/v1729842193/iPhone_15_Pro_Natural_1_ltf9vr.webp')
+
+    def __str__(self):
+        self.name
+
+
 if __name__ == "__main__":
     with app.app_context():
         # db.create_all()
-        c1 = Category(name="Mobile")
-        c2 = Category(name="Tablet")
-        c3 = Category(name="Laptop")
-        db.session.add_all([c1, c2, c3])
+        # c1 = Category(name="Mobile")
+        # c2 = Category(name="Tablet")
+        # c3 = Category(name="Laptop")
+        # db.session.add_all([c1, c2, c3])
+        # import json
+        #
+        # with open('data/products.json', encoding='utf-8') as f:
+        #     products = json.load(f)
+        #     for p in products:
+        #         prod = Product(**p)
+        #         db.session.add(prod)
+        #
+        import hashlib
+
+        password = str(hashlib.md5("123".encode('utf-8')).hexdigest())
+
+        u = User(name='Nhat', username='user', password=password)
+        db.session.add(u)
         db.session.commit()
